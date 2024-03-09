72b694578c392658e209a168112d9d426363c452
jorgenwh <jorgenwhen@gmail.com>
2024-03-09 02:52:04 +0100

    bugfix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, json, request, jsonify
 from flask_cors import CORS
-
+import logging
+from datetime import datetime
 
 from barrier import OpenAiApiHandler, AnthropicApiHandler
 
@@ -11,6 +12,11 @@ CORS(app)
 openai_api_handler = OpenAiApiHandler()
 anthropic_api_handler = AnthropicApiHandler()
 
+logging.basicConfig(
+        filename="logs/" + str(datetime.now()) + "_session.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 @app.route("/")
 def root():
@@ -24,9 +30,9 @@ def chat_prompt():
     model = data["model"]
     messages = data["messages"]
 
-    print("--- Received chat prompt with parameters ---")
-    print("model=" + model)
-    print("messages=" + messages)
+    logging.info("\33[1m--- Received chat prompt with parameters ---\33[0m")
+    logging.info("model=" + str(model))
+    logging.info("messages=" + str(messages))
 
     if model in ["gpt-3.5-turbo", "gpt-4-turbo-preview"]:
         completion = openai_api_handler.prompt_text_model(
@@ -41,8 +47,8 @@ def chat_prompt():
     else:
         return jsonify({"error": "Model not found"})
 
-    print("completion=" + completion)
-    return jsonify({"response": completion})
+    logging.info("completion=" + str(completion))
+    return jsonify({"response": str(completion)})
 
 @app.route("/image_prompt/", methods=["POST"])
 def image_prompt():
@@ -52,13 +58,13 @@ def image_prompt():
     prompt = data["prompt"]
     parameters = data["parameters"]
 
-    print("--- Received image prompt with parameters ---")
-    print("model=" + model)
-    print("parameters=" + parameters)
-    print("prompt=" + prompt)
+    logging.info("\33[1m--- Received image prompt with parameters ---\33[0m")
+    logging.info("model=" + str(model))
+    logging.info("parameters=" + str(parameters))
+    logging.info("prompt=" + str(prompt))
 
     if model in ["dall-e-3"]:
-        completion = openai_api_handler.prompt_image_model(
+        response = openai_api_handler.prompt_image_model(
                 model=model,
                 prompt=prompt,
                 parameters=parameters,
@@ -66,8 +72,9 @@ def image_prompt():
     else:
         return jsonify({"error": "Model not found"})
 
-    return jsonify({"response": completion})
+    logging.info("response=" + str(response))
+    return jsonify({"response": str(response)})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="192.168.0.87", port=5000)
